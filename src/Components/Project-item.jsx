c97be5896d2498f3f53ef2ad4a5e58d27db359c9
Austin LB Chiwambo <austinlbchiwambo@macbookpro.home>
2025-06-12 22:49:56 +0100

    Update ProjectCard component with conditional link rendering and CSS tweaks

--- src/Components/Project-item.jsx
+++ src/Components/Project-item.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './ProjectCard.css';
 
-function ProjectCard({ title, description, tech }) {
+function ProjectCard({ title, description, tech,link }) {
   return (
     <div className="project-card">
       <h2>{title}</h2>
       <p>{description}</p>
+      {link && (
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          Live demo
+        </a>
+      )}
       <div className="tech-stack">{tech}</div>
     </div>
   );
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -39,6 +39,12 @@ function Project() {
         description="Developed a dynamic full-stack portfolio built with React and Django, featuring animated UI elements, a downloadable resume, and a modern design to showcase my skills, experience, and projects."
         tech="React · Python/Django"
       />
+       <ProjectCard
+        title="Koalocator"
+        description="Built an interactive koala habitat locator using React.js and Leaflet.js, enabling users to visualize wild koala density across Queensland. Integrated government geospatial datasets to display heatmaps and habitat zones, with a postcode search for local sanctuary discovery. Designed a clean UI and optimized map rendering to support smooth, responsive exploration of wildlife data."
+        link='https://anilocator-qqul821u1-austins-projects-1868d765.vercel.app'
+        tech="React · Leaflet.js · GeoJson"
+      />
         </div>
        
 
